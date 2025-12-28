08a9d5691f4f43b1439e76e2ad92ea3b5b2c722d
Wakalala-droid <vwanheda@gmail.com>
2025-12-28 07:46:05 +0200

    updated flow

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
       <LightRays
         raysOrigin="top-center"
         raysColor="#ffffff"
-        raysSpeed={2.5}
-        lightSpread={0.4}
+        raysSpeed={1.4}
+        lightSpread={1.4}
         rayLength={4.4}
         followMouse={true}
         mouseInfluence={0.8}
-        noiseAmount={0.2}
-        distortion={0.044}
+        noiseAmount={0.1}
+        distortion={0.0}
         className="custom-rays"
       />
     </div>
